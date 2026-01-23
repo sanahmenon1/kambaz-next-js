d00ff20098f3916ffbcf1f72edc8de435ea83b2e
sanahmenon1 <menon.san@northeastern.edu>
2026-01-22 23:17:11 -0500

    fixing account->signin/signup/profile

--- app/(kambaz)/account/page.tsx
+++ app/(kambaz)/account/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 export default function AccountNavigation() {
  return (
    <div id="wd-account-navigation">
-     <Link href="signin"> Signin </Link> <br />
-     <Link href="signup"> Signup </Link> <br />
-     <Link href="profile"> Profile </Link> <br />
+     <Link href="/account/signin"> Signin </Link> <br />
+     <Link href="/account/signup"> Signup </Link> <br />
+     <Link href="/account/profile"> Profile </Link> <br />
    </div>
 );}
