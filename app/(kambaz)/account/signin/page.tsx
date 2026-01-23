b284a192d5a437421abb9edaca37716f21f67c53
sanahmenon1 <menon.san@northeastern.edu>
2026-01-22 23:58:15 -0500

    name visible

--- app/(kambaz)/account/signin/page.tsx
+++ app/(kambaz)/account/signin/page.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 export default function Signin() {
  return (
     <div id="wd-signin-screen">
+    <h1>Sanah Menon </h1>
       <h3>Sign in</h3>
       <input className="wd-username" placeholder="username" /> <br />
       <input className="wd-password" placeholder="password" type="password" /> <br />
       <Link id="wd-signin-btn" href="/dashboard"> Sign in </Link> <br />
       <Link id="wd-signup-link" href="signup"> Sign up </Link>
+
+      <h6>Section 02: Jose Annunziato</h6>
     </div>
 );}
