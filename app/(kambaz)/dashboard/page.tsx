cd3219b0f765d1b573a61556602def4ebe191f6c
sanahmenon1 <menon.san@northeastern.edu>
2026-02-06 12:32:02 -0500

    everything for assignment 2

--- app/(kambaz)/dashboard/page.tsx
+++ app/(kambaz)/dashboard/page.tsx
@@ -1,142 +1,159 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Row, Col, Card, CardImg, CardBody, CardTitle, CardText, Button } from "react-bootstrap";
+
 export default function Dashboard() {
   return (
     <div id="wd-dashboard">
       <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
       <h2 id="wd-dashboard-published">Published Courses (8)</h2> <hr />
       <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link href="/courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/reactjs.png" width={200} height={150} alt="reactjs" />
-            <div>
-              <h5> CS1234 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer
-              </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course"> 
-            <Link href="/courses/4550" className="wd-dashboard-course-link">
-                <Image
-                src="/images/webdev.png"
-                width={200}
-                height={150}
-                alt="web development"
-                />
-                <div>
-                <h5>CS4550 Web Development</h5>
-                <p className="wd-dashboard-course-title">
+        <Row xs={1} md={5} className="g-4">
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link href="/courses/1234/home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/reactjs.png" width="100%" height={160}/>
+                <CardBody>
+                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                    CS1234 React JS
+                  </CardTitle>
+                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                    Full Stack software developer
+                  </CardText>
+                  <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link href="/courses/4550/home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/webdev.png" width="100%" height={160}/>
+                <CardBody>
+                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                    CS4550 Web Development
+                  </CardTitle>
+                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
                     Building modern full-stack web apps
-                </p>
-                <button>Go</button>
-                </div>
-            </Link>
-        </div>
-        <div className="wd-dashboard-course"> 
-            <Link href="/courses/2500" className="wd-dashboard-course-link">
-                <Image
-                src="/images/fundies.png"
-                width={200}
-                height={150}
-                alt="fundies"
-                />
-                <div>
-                <h5>CS2500 Fundamentals of CS</h5>
-                <p className="wd-dashboard-course-title">
+                  </CardText>
+                  <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link href="/courses/2500/home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/fundies.png" width="100%" height={160}/>
+                <CardBody>
+                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                    CS2500 Fundamentals of CS
+                  </CardTitle>
+                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
                     Program design and problem solving
-                </p>
-                <button>Go</button>
-                </div>
-            </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/courses/1342" className="wd-dashboard-course-link">
-            <Image
-              src="/images/calculus.png"
-              width={200}
-              height={150}
-              alt="calculus"
-            />
-            <div>
-              <h5>MATH1342 Calculus II</h5>
-              <p className="wd-dashboard-course-title">
-                Integration techniques and applications
-              </p>
-              <button>Go</button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/courses/2510" className="wd-dashboard-course-link">
-            <Image
-              src="/images/ood.png"
-              width={200}
-              height={150}
-              alt="object oriented design"
-            />
-            <div>
-              <h5>CS2510 Fundamentals of OOD</h5>
-              <p className="wd-dashboard-course-title">
-                Object-oriented design and abstraction
-              </p>
-              <button>Go</button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/courses/3000" className="wd-dashboard-course-link">
-            <Image
-              src="/images/algorithms.png"
-              width={200}
-              height={150}
-              alt="algorithms"
-            />
-            <div>
-              <h5>CS3000 Algorithms</h5>
-              <p className="wd-dashboard-course-title">
-                Algorithm design and analysis
-              </p>
-              <button>Go</button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link href="/courses/3800" className="wd-dashboard-course-link">
-            <Image
-              src="/images/theory.png"
-              width={200}
-              height={150}
-              alt="theory of computation"
-            />
-            <div>
-              <h5>CS3800 Theory of Computation</h5>
-              <p className="wd-dashboard-course-title">
-                Automata, languages, and computability
-              </p>
-              <button>Go</button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-            <Link href="/courses/2550" className="wd-dashboard-course-link">
-                <Image
-                src="/images/cybersecurity.png"
-                width={200}
-                height={150}
-                alt="cybersecurity"
-                />
-                <div>
-                <h5>CY2550 Foundations of Cybersecurity</h5>
-                <p className="wd-dashboard-course-title">
+                  </CardText>
+                  <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link href="/courses/1342/home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/calculus.png" width="100%" height={160}/>
+                <CardBody>
+                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                    MATH1342 Calculus II
+                  </CardTitle>
+                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                    Integration techniques and applications
+                  </CardText>
+                  <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link href="/courses/2510/home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/ood.png" width="100%" height={160}/>
+                <CardBody>
+                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                    CS2510 Fundamentals of OOD
+                  </CardTitle>
+                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                    Object-oriented design and abstraction
+                  </CardText>
+                  <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link href="/courses/3000/home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/algorithms.png" width="100%" height={160}/>
+                <CardBody>
+                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                    CS3000 Algorithms
+                  </CardTitle>
+                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                    Algorithm design and analysis
+                  </CardText>
+                  <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link href="/courses/3800/home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/theory.png" width="100%" height={160}/>
+                <CardBody>
+                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                    CS3800 Theory of Computation
+                  </CardTitle>
+                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
+                    Automata, languages, and computability
+                  </CardText>
+                  <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+
+          <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+            <Card>
+              <Link href="/courses/2550/home"
+                className="wd-dashboard-course-link text-decoration-none text-dark">
+                <CardImg variant="top" src="/images/cybersecurity.png" width="100%" height={160}/>
+                <CardBody>
+                  <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                    CY2550 Foundations of Cybersecurity
+                  </CardTitle>
+                  <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
                     Security principles, threats, and defenses
-                </p>
-                <button>Go</button>
-                </div>
-            </Link>
-        </div>
+                  </CardText>
+                  <Button variant="primary">Go</Button>
+                </CardBody>
+              </Link>
+            </Card>
+          </Col>
+        </Row>
       </div>
     </div>
-);}
+  );
+}
--- app/(kambaz)/navigation.tsx
+++ app/(kambaz)/navigation.tsx
@@ -1,13 +1,64 @@
+"use client";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function KambazNavigation() {
+  const pathname = usePathname();
+  
   return (
-    <div id="wd-kambaz-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
-      <Link href="/account" id="wd-account-link">Account</Link><br/>
-      <Link href="/dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      <Link href="/dashboard" id="wd-course-link">Courses</Link><br/>
-      <Link href="/calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link href="/inbox" id="wd-inbox-link">Inbox</Link><br/>
-      <Link href="/labs" id="wd-labs-link">Labs</Link><br/>
-    </div>
-);}
+    <ListGroup className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2" style={{ width: 120 }}
+      id="wd-kambaz-navigation">
+      <ListGroupItem className="bg-black border-0 text-center" as="a"
+        target="_blank" href="https://www.northeastern.edu/" id="wd-neu-link">
+        <img src="/images/NEU.png" width="75px" alt="Northeastern University" />
+      </ListGroupItem>
+      <ListGroupItem className={`border-0 text-center ${pathname.includes("/account") ? "bg-white" : "bg-black"}`}>
+        <Link href="/account" id="wd-account-link" className="text-decoration-none">
+          <FaRegCircleUser className={`fs-1 ${pathname.includes("/account") ? "text-danger" : "text-white"}`} />
+          <br />
+          <span className={pathname.includes("/account") ? "text-danger" : "text-white"}>Account</span>
+        </Link>
+      </ListGroupItem>
+      <ListGroupItem className={`border-0 text-center ${pathname.includes("/dashboard") ? "bg-white" : "bg-black"}`}>
+        <Link href="/dashboard" id="wd-dashboard-link" className="text-decoration-none">
+          <AiOutlineDashboard className="fs-1 text-danger" />
+          <br />
+          <span className="text-danger">Dashboard</span>
+        </Link>
+      </ListGroupItem>
+      <ListGroupItem className={`border-0 text-center ${pathname.includes("/courses") ? "bg-white" : "bg-black"}`}>
+        <Link href="/courses" id="wd-course-link" className="text-decoration-none">
+          <LiaBookSolid className="fs-1 text-danger" />
+          <br />
+          <span className="text-danger">Courses</span>
+        </Link>
+      </ListGroupItem>
+      <ListGroupItem className={`border-0 text-center ${pathname.includes("/calendar") ? "bg-white" : "bg-black"}`}>
+        <Link href="/calendar" id="wd-calendar-link" className="text-decoration-none">
+          <IoCalendarOutline className="fs-1 text-danger" />
+          <br />
+          <span className="text-danger">Calendar</span>
+        </Link>
+      </ListGroupItem>
+      <ListGroupItem className={`border-0 text-center ${pathname.includes("/inbox") ? "bg-white" : "bg-black"}`}>
+        <Link href="/inbox" id="wd-inbox-link" className="text-decoration-none">
+          <FaInbox className="fs-1 text-danger" />
+          <br />
+          <span className="text-danger">Inbox</span>
+        </Link>
+      </ListGroupItem>
+      <ListGroupItem className={`border-0 text-center ${pathname.includes("/labs") ? "bg-white" : "bg-black"}`}>
+        <Link href="/labs" id="wd-labs-link" className="text-decoration-none">
+          <LiaCogSolid className="fs-1 text-danger" />
+          <br />
+          <span className="text-danger">Labs</span>
+        </Link>
+      </ListGroupItem>
+    </ListGroup>
+  );
+}
